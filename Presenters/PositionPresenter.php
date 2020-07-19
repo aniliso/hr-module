bb3d759ad28b88c0e55dd628d99cf6af2a6d64b4
sagsoz06 <designer@visualturk.net>
2020-07-20 01:41:33 +0300

    Fixed presenter

--- Presenters/PositionPresenter.php
+++ Presenters/PositionPresenter.php
@@ -22,14 +22,14 @@ class PositionPresenter extends BasePresenter
         if(isset($this->entity->position->{$field})) {
             if($field == "city" && isset($this->entity->position->city)) {
                 $cities = City::whereIn('id', $this->entity->position->city)->get()->pluck('name', 'id')->toArray();
-                if(count($cities)>0) {
+                if(is_array($cities)) {
                     return implode(', ', $cities);
                 }
                 return null;
             } elseif(isset($this->entity->position->{$field})) {
-                if(count($this->entity->position->{$field})>0) {
+                if(is_array($this->entity->position->{$field})>0) {
                     $information = $this->information->{$field}()->selected($this->entity->position->{$field});
-                    if(count($information)>0) {
+                    if(is_array($information)) {
                         return implode(', ', $information);
                     }
                 }
@@ -43,7 +43,7 @@ class PositionPresenter extends BasePresenter
     public function criteria($field="")
     {
         if(isset($this->entity->criteria->{$field})) {
-            if(count($this->entity->criteria->{$field})>0) {
+            if(is_array($this->entity->criteria->{$field})) {
                 $criteria = $this->criteria->{$field}()->selected($this->entity->criteria->{$field});
                 if(count($criteria)>0) {
                     return implode(', ', $criteria);
